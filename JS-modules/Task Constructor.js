60ca6cd5519473c21777f887d8b10e73eb291a99
unknown <stevenhnguyen0@gmail.com>
2022-11-28 10:57:12 +1100

    Add tasks to page WIP

--- JS-modules/Task Constructor.js
+++ JS-modules/Task Constructor.js
@@ -159,3 +159,62 @@ function fnd005() {
 
 //code below just for checking if its working
 //setInterval(()=>{console.log(inputRad)}, 2000);
+
+/*
+window.onload = loadTasks;
+
+function loadTasks(){
+    let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
+    
+
+    tasks.forEach(task => {
+        const list = document.querySelector("taskcont");
+        const li = document.querySelector("taskL");
+        getAllInputs();
+        taskL.innerHTML= '';
+
+        list.insertBefore(li, list.children[0]);
+    });
+}
+*/
+
+
+/*
+window.onload = loadTasks;
+
+function loadTasks() {
+  // Get the tasks from localStorage and convert it to an array
+  let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
+
+  // Loop through the tasks and add them to the list
+  tasks.forEach(task => {
+    const list = document.querySelector("ul");
+    const li = document.createElement("li");
+    li.innerHTML = `<input type="checkbox" onclick="taskComplete(this)" class="check" ${task.completed ? 'checked' : ''}>
+          <input type="text" value="${task.task}" class="task ${task.completed ? 'completed' : ''}" onfocus="getCurrentTask(this)" onblur="editTask(this)">
+          <i class="fa fa-trash" onclick="removeTask(this)"></i>`;
+    list.insertBefore(li, list.children[0]);
+  });
+}
+*/
+const getAllInputs = () =>{
+    'taskName':inputTask.value,
+    'taskDescription':inputDesc.value,
+    'taskAssignedTo':inputAss.value,
+    'taskDueDate':inputDate.value,
+    'taskStatus':inputRad
+}
+
+function addTasks(){
+    addTask(getAllInputs());
+}
+
+
+
+
+document.querySelector("#taskcont").addEventListener("submit", e =>{
+    e.preventDefault();
+    addTasks();
+
+});
+
