226a3ffa2a8142b89654358c9fad6c271d99b8ab
Cameron <Cameron.Pubben@gmail.com>
2022-12-01 12:04:21 +1030

    Adjusted time and date to display numbers below 10, and added AM & PM

--- JS-modules/DateTime.js
+++ JS-modules/DateTime.js
@@ -1,13 +1,25 @@
 setInterval(() =>{
-const date = new Date();
+    const date = new Date();
 
-let day = date.getDate();
-let month = date.getMonth() + 1;
-let year = date.getFullYear();
-let hour = date.getHours();
-let min = date.getMinutes();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    let session = "AM";
 
-let currentDate = `Current Time ${hour}:${min} <br>Todays Date ${day}-${month}-${year}`;
+    if(hour == 0){
+        hour = 12;
+    }
+    if(hour > 12){
+        hour = hh - 12;
+        session = "PM";
+    }
+
+    hour = (hour < 10) ? "0" + hour : hour;
+    min = (min <10) ? "0" + min : min;
+
+let currentDate = `Current Time ${hour}:${min} ${session} <br>Todays Date ${day}-${month}-${year}`;
 const dateH = document.getElementById('time');
 dateH.innerHTML = `${currentDate}`;
 }, 1000)
