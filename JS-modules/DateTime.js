9aa48401e1e248596a88230530dc4ab8282b3342
unknown <stevenhnguyen0@gmail.com>
2022-12-01 13:49:25 +1100

    Reformatted and cleaned up code

--- JS-modules/DateTime.js
+++ JS-modules/DateTime.js
@@ -1,4 +1,4 @@
-setInterval(() =>{
+setInterval(() => {
     const date = new Date();
 
     let day = date.getDate();
@@ -8,18 +8,18 @@ setInterval(() =>{
     let min = date.getMinutes();
     let session = "AM";
 
-    if(hour == 0){
+    if (hour == 0) {
         hour = 12;
     }
-    if(hour > 12){
+    if (hour > 12) {
         hour = hour - 12;
         session = "PM";
     }
 
     hour = (hour < 10) ? "0" + hour : hour;
-    min = (min <10) ? "0" + min : min;
+    min = (min < 10) ? "0" + min : min;
 
-let currentDate = `Current Time ${hour}:${min} ${session} <br>Todays Date ${day}-${month}-${year}`;
-const dateH = document.getElementById('time');
-dateH.innerHTML = `${currentDate}`;
+    let currentDate = `Current Time ${hour}:${min} ${session} <br>Todays Date ${day}-${month}-${year}`;
+    const dateH = document.getElementById('time');
+    dateH.innerHTML = `${currentDate}`;
 }, 1000);
